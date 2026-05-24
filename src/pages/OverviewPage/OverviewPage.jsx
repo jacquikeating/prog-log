ba680e92e4efeb651242224df0a79d8c38013d7a
jacquikeating <j.keating@live.ca>
2026-05-23 23:11:26 -0400

    feat: Create OverviewPage

---
+++ src/pages/OverviewPage/OverviewPage.jsx
@@ -0,0 +1,9 @@
+const OverviewPage = () => {
+  return (
+    <main className="overview-page">
+      <h1 className="overview-page__title">Overview</h1>
+    </main>
+  );
+};
+
+export default OverviewPage;
