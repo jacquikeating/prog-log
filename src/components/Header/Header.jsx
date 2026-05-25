4432318e45ced07fdb994f3a75c316795a90f49b
jacquikeating <j.keating@live.ca>
2026-05-24 23:36:44 -0400

    Feat: Recreate links in Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -10,6 +10,43 @@ const Header = () => {
             <img src="https://i.imgur.com/GNPU7aE.png"></img>
             <p className="nav__site-name">ProgLog</p>
           </Link>
+          <ul className="nav__list">
+            <li className="nav__list-item">
+              <Link to="/" className="nav__link">
+                Home
+              </Link>
+            </li>
+            <li className="nav__list-item">
+              {/* <Link to={`/report/${latestSession}`} className="nav__link"> */}
+              <Link to={`/report/x`} className="nav__link"> {/* Temp */}
+                Latest
+              </Link>
+            </li>
+            <li className="nav__list-item">
+              <Link to="/about" className="nav__link">
+                About
+              </Link>
+            </li>
+            <li className="nav__list-item">
+              <Link to="/clips" className="nav__link">
+                Clips
+              </Link>
+            </li>
+            <li className="nav__list-item">
+              <Link to="/timeline" className="nav__link">
+                Timeline
+              </Link>
+            </li>
+            {/* {role === "admin" ? (
+              <li className="nav__list-item">
+                <Link to="/add-data" className="nav__link">
+                  Add Data
+                </Link>
+              </li>
+            ) : (
+              ""
+            )} */}
+          </ul>
         </div>
         <div className="nav__group nav__group--secondary">
         </div>
