2480f6f067e6f9703c82b6698173c616a13c3820
jacquikeating <j.keating@live.ca>
2026-05-30 00:47:19 +0600

    style: Code formatting improvements

--- src/pages/OverviewPage/OverviewPage.jsx
+++ src/pages/OverviewPage/OverviewPage.jsx
@@ -5,13 +5,13 @@ import SessionsList from "../../components/SessionsList/SessionsList";
 const OverviewPage = ({ sessions, pulls }) => {
     return (
         <main className="overview-page">
-        <h1 className="overview-page__title">Overview</h1>
-        <OverviewStats sessions={sessions} pulls={pulls} />
-        <PhaseBreakdownTable
-            progPhase={sessions[0].prog_phase}
-            pulls={pulls} 
-        />
-        <SessionsList sessions={sessions} />
+            <h1 className="overview-page__title">Overview</h1>
+            <OverviewStats sessions={sessions} pulls={pulls} />
+            <PhaseBreakdownTable
+                progPhase={sessions[0].prog_phase}
+                pulls={pulls} 
+            />
+            <SessionsList sessions={sessions} />
         </main>
     );
 };
