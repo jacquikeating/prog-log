437c2cdc37562e7b0269b236f75c49ed13906fbc
jacquikeating <j.keating@live.ca>
2026-05-31 22:53:33 -0400

    feat: Recreate AddDataPage

---
+++ src/pages/AddDataPage/AddDataPage.jsx
@@ -0,0 +1,162 @@
+import { useState, useEffect } from "react";
+// import axios from "axios";
+import { createReadableDate } from "../../utils/shared-functions.js";
+// import NewSessionForm from "../../components/NewSessionForm/NewSessionForm";
+// import NewPullForm from "../../components/NewPullForm/NewPullForm";
+import PullsTable from "../../components/PullsTable/PullsTable";
+
+// const API_URL = import.meta.env.VITE_API_URL;
+
+const AddDataPage = () => {
+  const [sessionInProgress, setSessionInProgress] = useState(false);
+  const [sessionData, setSessionData] = useState({});
+  const [lastSession, setLastSession] = useState({});
+//   const lsPullsArray = JSON.parse(localStorage.getItem("pullsFromNewSession"));
+//   const [pullsArray, setPullsArray] = useState(lsPullsArray || []);
+
+//   useEffect(() => {
+//     const lsSessionData = JSON.parse(localStorage.getItem("sessionInProgress"));
+//     if (lsSessionData) {
+//       setSessionData(lsSessionData);
+//       setSessionInProgress(true);
+//     } else {
+//       async function getLastSessionData() {
+//         try {
+//           let result = await axios.get(`${API_URL}/sessions/`);
+//           let mostRecentSession = result.data.reverse()[0];
+//           setLastSession(mostRecentSession);
+//         } catch (error) {
+//           console.error(error);
+//         }
+//       }
+//       getLastSessionData();
+//     }
+//   }, []);
+
+  function handleSessionFormData(data) {
+    setSessionData(data);
+    setSessionInProgress(true);
+  }
+
+//   function handlePullFormData(data) {
+//     let copyOfPullsArray = [...pullsArray];
+//     if (data.indexToInsert == 0) {
+//       copyOfPullsArray.push(data);
+//     } else {
+//       copyOfPullsArray.splice(data.indexToInsert - 1, 0, data);
+//     }
+//     setPullsArray(copyOfPullsArray);
+//     localStorage.setItem(
+//       "pullsFromNewSession",
+//       JSON.stringify(copyOfPullsArray)
+//     );
+//   }
+
+//   function updatePull(pullData) {
+//     let copyOfPullsArray = [...pullsArray];
+//     copyOfPullsArray[pullData.index] = pullData;
+//     localStorage.setItem(
+//       "pullsFromNewSession",
+//       JSON.stringify(copyOfPullsArray)
+//     );
+//     setPullsArray(copyOfPullsArray);
+//   }
+
+//   function deletePull(pullData) {
+//     let copyOfPullsArray = [...pullsArray];
+//     copyOfPullsArray.splice(pullData.index, 1);
+//     localStorage.setItem(
+//       "pullsFromNewSession",
+//       JSON.stringify(copyOfPullsArray)
+//     );
+//     setPullsArray(copyOfPullsArray);
+//   }
+
+  async function handleSubmit() {
+    let counter = Number(localStorage.getItem("counter"));
+
+    pullsArray.map(async (pull, index) => {
+      pull.pull_num_today = Number(index + 1);
+      pull.pull_num_overall = counter + pull.pull_num_today;
+      delete pull.index;
+      delete pull.indexToInsert;
+      try {
+        await axios.post(`${API_URL}/pulls/`, pull);
+      } catch (error) {
+        console.error(error);
+      }
+    });
+
+    const lastPullNumOverall = pullsArray[pullsArray.length - 1].pull_num_overall;
+    localStorage.setItem("counter", lastPullNumOverall);
+    navigator.clipboard.writeText(localStorage.getItem("pullsFromNewSession"));
+    localStorage.removeItem("pullsFromNewSession");
+    localStorage.removeItem("sessionInProgress");
+    // navigate(`/report/${sessionData.num}`);
+  }
+
+  return (
+    <main className="add-data">
+      <h1 className="add-data__heading">Add Data</h1>
+      {!sessionInProgress ? (
+        <section className="add-data__section">
+          <h2 className="add-data__section-heading">New Session</h2>
+          {lastSession.id > 0 ? (
+            // <NewSessionForm
+            //   lastSession={lastSession}
+            //   handleSessionFormData={handleSessionFormData}
+            // />
+            <p>New session form goes here</p>
+          ) : (
+            ""
+          )}
+        </section>
+      ) : (
+        <>
+          <section className="add-data__section">
+            <h2 className="add-data__section-heading">Session Info</h2>
+            <p>Session: {sessionData.num}</p>
+            <p>Date: {createReadableDate(sessionData.date)}</p>
+            <p>Roster: {sessionData.roster}</p>
+            <p>
+              Prog Point: Phase {sessionData.prog_phase},{" "}
+              {sessionData.prog_mech}
+            </p>
+            <p>Goal: {sessionData.goal}</p>
+            <p>Notes: {sessionData.notes}</p>
+            <p>FFLogs Link: {sessionData.fflogs_link}</p>
+            <p>Twitch Links: {sessionData.twitch_links}</p>
+          </section>
+          <section className="add-data__section">
+            <h2 className="add-data__section-heading">Add a Pull</h2>
+            {/* <NewPullForm
+              sessionData={sessionData}
+              pullsArray={pullsArray}
+              handlePullFormData={handlePullFormData}
+            /> */}
+            <p>New pull form goes here</p>
+          </section>
+          <section className="add-data__section">
+            <h2 className="add-data__section-heading">Pulls</h2>
+            <PullsTable
+              pullsArray={pullsArray}
+              showEdit={true}
+              deletePull={deletePull}
+              updatePull={updatePull}
+              progPhase={sessionData.prog_phase}
+              allowDelete={true}
+            />
+          </section>
+          <section className="add-data__section">
+            <button className="add-data__button" onClick={handleSubmit}>
+              Complete Session
+            </button>
+            {/* <button onClick={resurrectPull}>bring the pull back</button> */}
+          </section>
+        </>
+      )}
+    </main>
+  );
+};
+
+export default AddDataPage;
