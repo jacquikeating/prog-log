a35c6d5344435321e75a32c7ad9da6f59683331f
jacquikeating <j.keating@live.ca>
2026-06-02 12:48:08 -0400

    feat: Add selects to NewPullForm

--- src/components/NewPullForm/NewPullForm.jsx
+++ src/components/NewPullForm/NewPullForm.jsx
@@ -19,11 +19,11 @@ const NewPullForm = ({ sessionData, handlePullFormData }) => {
     const phaseAndMechOptions = getPhaseAndMechOptions(sessionData.ulti);
 
     function handlePhaseChange(e) {
-        setSelectedPhase(e.phase);
+        setSelectedPhase(e.target.value);
     }
 
     function handleMechChange(e) {
-        setSelectedMech(e.mech);
+        setSelectedMech(e.target.value);
     }
 
     function handleInsert(e) {
@@ -75,198 +75,177 @@ const NewPullForm = ({ sessionData, handlePullFormData }) => {
 
     return (
         <form className="form">
-        <label className="form__label" htmlFor="phase">
-            Phase & Mechanic
-        </label>
+            {/* <label className="form__label" htmlFor="phase">
+                Phase & Mechanic
+            </label> */}
 
-        <div className="form__pickers-container">
-            <Picker
-                value={selectedPhase}
-                onChange={handlePhaseChange}
-                wheelMode="natural"
-                height={90}
-                itemHeight={30}
-                className="form__picker"
-            >
-                <Picker.Column key="phase" name="phase">
-                    {phaseAndMechOptions.map((_phase, index) => (
-                        <Picker.Item
-                            key={index}
-                            value={index}
-                            className="form__picker-option"
-                            style={selectedPhase == index && {color: "#b38cff"}}
-                        >
-                            {index}
-                        </Picker.Item>
-                    ))}
-                </Picker.Column>
-            </Picker>
+            {/* <div className="form__pickers-container">
+                <Picker
+                    value={selectedPhase}
+                    onChange={handlePhaseChange}
+                    wheelMode="natural"
+                    height={90}
+                    itemHeight={30}
+                    className="form__picker"
+                >
+                    <Picker.Column key="phase" name="phase">
+                        {phaseAndMechOptions.map((_phase, index) => (
+                            <Picker.Item
+                                key={index}
+                                value={index}
+                                className="form__picker-option"
+                                style={selectedPhase == index && {color: "#b38cff"}}
+                            >
+                                {index}
+                            </Picker.Item>
+                        ))}
+                    </Picker.Column>
+                </Picker>
 
-            <Picker
-                value={selectedMech}
-                onChange={handleMechChange}
-                wheelMode="natural"
-                height={90}
-                itemHeight={30}
-                className="form__picker"
-            >
-                <Picker.Column key="mech" name="mech">
-                    {phaseAndMechOptions[selectedPhase].map((mech) => (
-                    <Picker.Item
-                        key={mech}
-                        value={mech}
-                        className="form__picker-option"
-                        style={selectedMech == mech && {color: "#b38cff"}}
-                    >
-                        {mech}
-                    </Picker.Item>
-                    ))}
-                </Picker.Column>
-            </Picker>
-        </div>
-        {/*
-            <select className="form__select" name="phase" id="phase">
-            <option value="1" className="form__option">
-                1
-            </option>
-            <option value="2" className="form__option">
-                2
-            </option>
-            <option value="3" className="form__option">
-                3
-            </option>
-            <option value="4" className="form__option">
-                4
-            </option>
-            <option value="5" className="form__option">
-                5
-            </option>
-            <option value="6" className="form__option">
-                6
-            </option>
-            <option value="7" className="form__option">
-                7
-            </option>
-            </select>
+                <Picker
+                    value={selectedMech}
+                    onChange={handleMechChange}
+                    wheelMode="natural"
+                    height={90}
+                    itemHeight={30}
+                    className="form__picker"
+                >
+                    <Picker.Column key="mech" name="mech">
+                        {phaseAndMechOptions[selectedPhase].map((mech) => (
+                            <Picker.Item
+                                key={mech}
+                                value={mech}
+                                className="form__picker-option"
+                                style={selectedMech == mech && {color: "#b38cff"}}
+                            >
+                                {mech}
+                            </Picker.Item>
+                        ))}
+                    </Picker.Column>
+                </Picker>
+            </div> */}
 
-            <label className="form__label" htmlFor="mech">
-            Mechanic
-            </label>
-            <select className="form__select" name="mech" id="mech">
-            <option value="A" className="form__option">
-                A
-            </option>
-            <option value="B" className="form__option">
-                B
-            </option>
-            <option value="C" className="form__option">
-                C
-            </option>
-            </select> */}
-
-        <label className="form__label" htmlFor="cause">
-            Cause of Wipe
-        </label>
-        <input
-            className="form__input form__input--text"
-            type="text"
-            name="cause"
-            id="cause"
-            value={cause}
-            onChange={(e) => {
-            setCause(e.target.value);
-            }}
-        />
-
-        {rosterArray.length ? (
-            <fieldset className="form__fieldset">
-            <legend className="form__label">Players Involved</legend>
-            {rosterArray.map((player, index) => {
-                return (
-                <label className="form__label" htmlFor={player} key={index}>
-                    <input
-                    className="form__checkbox"
-                    type="checkbox"
-                    name={player}
-                    id={player}
-                    value={player}
-                    checked={checkedState[index]}
-                    onChange={() => handleCheckboxChange(index)}
-                    />
-                    {player}
+            <div className="form__selects-container">
+                <label className="form__label" htmlFor="phase">
+                    Phase
                 </label>
-                );
-            })}
-            </fieldset>
-        ) : (
-            "Loading..."
-        )}
+                <select className="form__select" name="phase" id="phase" onChange={handlePhaseChange} value={selectedPhase}>
+                    {phaseAndMechOptions.map((_phase, index) => {
+                        if (!index == 0) { 
+                            return (
+                                <option value={index} className="form__option">
+                                    {index}
+                                </option>
+                            )
+                        } 
+                    })}
+                </select>
 
-        <label className="form__label" htmlFor="log-link">
-            Log
-        </label>
-        <input
-            className="form__input form__input--text"
-            type="text"
-            name="log-link"
-            id="log-link"
-            value={logLink}
-            onChange={(e) => {
-            setLogLink(e.target.value);
-            }}
-        />
+                <label className="form__label" htmlFor="mech">
+                    Mechanic
+                </label>
+                <select className="form__select" name="mech" id="mech"  onChange={handleMechChange} value={selectedMech}>
+                    {phaseAndMechOptions[selectedPhase].map((mech) => {
+                        return (
+                            <option value={mech} className="form__option">
+                                {mech}
+                            </option>
+                        ) 
+                    })}
+                </select>
+            </div>
 
-        <label className="form__label" htmlFor="clip-link">
-            Clip
-        </label>
-        <input
-            className="form__input form__input--text"
-            type="text"
-            name="clip-link"
-            id="clip-link"
-            value={clipLink}
-            onChange={(e) => {
-            setClipLink(e.target.value);
-            }}
-        />
+            <label className="form__label" htmlFor="cause">
+                Cause of Wipe
+            </label>
+            <input
+                className="form__input form__input--text"
+                type="text"
+                name="cause"
+                id="cause"
+                value={cause}
+                onChange={(e) => {setCause(e.target.value)}}
+            />
 
-        <label className="form__label" htmlFor="notes">
-            Notes
-        </label>
-        <input
-            className="form__input form__input--text"
-            type="text"
-            name="notes"
-            id="notes"
-            value={notes}
-            onChange={(e) => {
-            setNotes(e.target.value);
-            }}
-        />
+            {rosterArray.length ? (
+                <fieldset className="form__fieldset">
+                    <legend className="form__label">Players Involved</legend>
+                    {rosterArray.map((player, index) => {
+                        return (
+                        <label className="form__label" htmlFor={player} key={index}>
+                            <input
+                                className="form__checkbox"
+                                type="checkbox"
+                                name={player}
+                                id={player}
+                                value={player}
+                                checked={checkedState[index]}
+                                onChange={() => handleCheckboxChange(index)}
+                            />
+                            {player}
+                        </label>
+                        );
+                    })}
+                </fieldset>
+            ) : (
+                "Loading..."
+            )}
 
-        <button type="submit" className="form__button" onClick={handleSubmit}>
-            Save
-        </button>
+            <label className="form__label" htmlFor="log-link">
+                Log
+            </label>
+            <input
+                className="form__input form__input--text"
+                type="text"
+                name="log-link"
+                id="log-link"
+                value={logLink}
+                onChange={(e) => {setLogLink(e.target.value)}}
+            />
 
-        <div>
-            <button className="form__button" onClick={handleInsert}>
-            Insert at...
+            <label className="form__label" htmlFor="clip-link">
+                Clip
+            </label>
+            <input
+                className="form__input form__input--text"
+                type="text"
+                name="clip-link"
+                id="clip-link"
+                value={clipLink}
+                onChange={(e) => {setClipLink(e.target.value)}}
+            />
+
+            <label className="form__label" htmlFor="notes">
+                Notes
+            </label>
+            <input
+                className="form__input form__input--text"
+                type="text"
+                name="notes"
+                id="notes"
+                value={notes}
+                onChange={(e) => {setNotes(e.target.value)}}
+            />
+
+            <button type="submit" className="form__button" onClick={handleSubmit}>
+                Save
             </button>
 
-            {!insertMode ? (
-            ""
-            ) : (
-            <>
-                <input
-                type="number"
-                value={indexToInsert}
-                onChange={(e) => {
-                    setIndexToInsert(e.target.value);
-                }}
-                />
-            </>
-            )}
-        </div>
+            <div>
+                <button className="form__button" onClick={handleInsert}>
+                    Insert at...
+                </button>
+
+                {!insertMode ? (
+                    ""
+                ) : (
+                    <input
+                        type="number"
+                        value={indexToInsert}
+                        onChange={(e) => {setIndexToInsert(e.target.value)}}
+                    />
+                )}
+            </div>
         </form>
     );
 };
