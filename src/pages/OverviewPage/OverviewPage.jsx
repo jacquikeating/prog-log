6aba99b5a360b785dc2f51c8d871df9fd3a55194
jacquikeating <j.keating@live.ca>
2026-06-03 18:26:01 -0400

    feat: OverviewPage filters sessions by staticNick param

--- src/pages/OverviewPage/OverviewPage.jsx
+++ src/pages/OverviewPage/OverviewPage.jsx
@@ -1,17 +1,23 @@
+import { useParams } from "react-router-dom";
 import OverviewStats from "../../components/OverviewStats/OverviewStats.jsx";
 import PhaseBreakdownTable from "../../components/PhaseBreakdownTable/PhaseBreakdownTable";
 import SessionsList from "../../components/SessionsList/SessionsList";
 
 const OverviewPage = ({ sessions, pulls }) => {
+    const { staticNick, ulti } = useParams();
+
+    const staticSessions = sessions.filter((session) => session.static == staticNick);
+    const filteredSessions = staticSessions.filter((session) => session.ulti.toLowerCase() == ulti);
+
     return (
         <main className="overview-page">
             <h1 className="overview-page__title">Overview</h1>
-            <OverviewStats sessions={sessions} pulls={pulls} />
+            <OverviewStats sessions={filteredSessions} pulls={pulls} />
             <PhaseBreakdownTable
-                progPhase={sessions[0].prog_phase}
+                progPhase={filteredSessions[0].prog_phase}
                 pulls={pulls} 
             />
-            <SessionsList sessions={sessions} />
+            <SessionsList sessions={filteredSessions} />
         </main>
     );
 };
