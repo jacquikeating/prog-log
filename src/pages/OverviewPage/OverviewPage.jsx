a3fe7b260355f7adab637807d555af4927e4d65d
jacquikeating <j.keating@live.ca>
2026-06-03 18:28:53 -0400

    feat: OverviewPage filters pulls by staticNick and ulti

--- src/pages/OverviewPage/OverviewPage.jsx
+++ src/pages/OverviewPage/OverviewPage.jsx
@@ -7,14 +7,15 @@ const OverviewPage = ({ sessions, pulls }) => {
     const { staticNick, ulti } = useParams();
 
     const filteredSessions = sessions.filter((session) => session.static == staticNick && session.ulti.toLowerCase() == ulti );
+    const filteredPulls = pulls.filter((pull) => pull.static == staticNick && pull.ulti.toLowerCase() == ulti );
 
     return (
         <main className="overview-page">
             <h1 className="overview-page__title">Overview</h1>
-            <OverviewStats sessions={filteredSessions} pulls={pulls} />
+            <OverviewStats sessions={filteredSessions} pulls={filteredPulls} />
             <PhaseBreakdownTable
                 progPhase={filteredSessions[0].prog_phase}
-                pulls={pulls} 
+                pulls={filteredPulls} 
             />
             <SessionsList sessions={filteredSessions} />
         </main>
