7368c0a81b5b1cdce896ffddec7c2f40032bb9dc
jacquikeating <j.keating@live.ca>
2026-06-15 23:55:26 -0400

    feat: Build basic SignupForm JSX structure

--- src/components/SignupForm/SignupForm.jsx
+++ src/components/SignupForm/SignupForm.jsx
@@ -1,8 +1,50 @@
 import "./SignupForm.scss";
 
 const SignupForm = ({}) => {
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        console.log("Form submitted");
+    }
     return (
-        <p>SignupForm</p>
+        <div className="signup">
+            <h2 className="signup__heading">Signup</h2>
+            <form className="signup__form" onSubmit={handleSubmit}>
+                <label>
+                    Email
+                    <input 
+                        type="email"
+                        name="email" 
+                        className="signup__text-input"
+                    />
+                </label>
+                <label>
+                    Confirm Email
+                    <input 
+                        type="email"
+                        name="email-confirm" 
+                        className="signup__text-input"
+                    />
+                </label>
+                <label>
+                    Password
+                    <input 
+                        type="password"
+                        name="password" 
+                        className="signup__text-input"
+                    />
+                </label>
+                <label>
+                    Confirm Password
+                    <input 
+                        type="password"
+                        name="password-confirm" 
+                        className="signup__text-input"
+                    />
+                </label>
+                <button type="submit" className="signup__submit">Submit</button>
+            </form>
+        </div>
     )
 };
 
