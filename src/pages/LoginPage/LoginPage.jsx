51881750c7f942b0598b44541f4b3b6887370de4
jacquikeating <j.keating@live.ca>
2026-06-26 17:18:45 -0400

    feat: Style LoginPage

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -6,7 +6,7 @@ import "./LoginPage.scss";
 const LoginPage = () => {
     return (
         <main className="login">
-            <h1>Login</h1>
+            <h1 className="login__heading">Login</h1>
             <LoginForm />
             <Link className="login__create-acct-link" to="/signup">Create an account</Link>
         </main>
