aeef239da30f600aee1fe4814032f748dd68b920
jacquikeating <j.keating@live.ca>
2026-06-30 01:27:39 +0300

    feat: Write logout function

--- src/pages/AccountPage/AccountPage.jsx
+++ src/pages/AccountPage/AccountPage.jsx
@@ -1,9 +1,28 @@
-import "./AccountPage.scss"
+import { supabase } from "../../supabase-client";
+import "./AccountPage.scss";
 
-const AccountPage = ({ }) => {
+const AccountPage = ({ loginData }) => {
+    let user = "";
+
+    if (loginData.session) {
+        user = loginData.session.user.user_metadata;
+        console.log(user);
+    };
+
+    async function logout() {
+        await supabase.auth.signOut();
+    };
+
     return (
         <main>
             <h1>Account</h1>
+            <section>
+                {loginData.session ? (
+                    <button onClick={logout}>Logout</button>
+                ) : (
+                    <p>You are not signed in.</p>
+                )}
+            </section>    
         </main>
     )
 };
