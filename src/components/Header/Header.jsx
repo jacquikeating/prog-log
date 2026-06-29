05637bfe48b523228dbe71fb2ca7e744ad5b47f6
jacquikeating <j.keating@live.ca>
2026-06-30 01:28:35 +0300

    fix: Specify loginData.session for Header's dynamic account/login link

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -54,7 +54,7 @@ const Header = ({ latestSession, loginData }) => {
         <div className="nav__group nav__group--secondary">
           <ul className="nav__list">
             <li className="nav__list-item">
-              {loginData ? (
+              {loginData.session ? (
                 <Link to="/account" className="nav__link">
                   Account
                 </Link>
