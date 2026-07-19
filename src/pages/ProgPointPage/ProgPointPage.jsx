aca12ad591c2f1ebdead7b0d1b5f474793433e8e
jacquikeating <j.keating@live.ca>
2026-07-18 23:28:26 -0600

    feat: Create ProgPointPage

---
+++ src/pages/ProgPointPage/ProgPointPage.jsx
@@ -0,0 +1,11 @@
+import "./ProgPointPage.scss";
+
+const ProgPointPage = () => {
+    return (
+        <main className="prog-point">
+            <h1 className="prog-point__heading">Prog Point: Mech Name Goes Here</h1>
+        </main>
+    );
+};
+
+export default ProgPointPage;
