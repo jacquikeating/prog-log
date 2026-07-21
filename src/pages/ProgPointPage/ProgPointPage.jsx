75be407022c261e969e58de21cb5e94f715618ee
jacquikeating <j.keating@live.ca>
2026-07-20 23:27:56 -0400

    feat: Implement useOutletContext in ProgPointPage

--- src/pages/ProgPointPage/ProgPointPage.jsx
+++ src/pages/ProgPointPage/ProgPointPage.jsx
@@ -1,9 +1,12 @@
+import { useOutletContext } from "react-router-dom";
 import "./ProgPointPage.scss";
 
 const ProgPointPage = () => {
+    const { sessions, pulls } = useOutletContext();
+
     return (
         <main className="prog-point">
-            <h1 className="prog-point__heading">Prog Point: Mech Name Goes Here</h1>
+            <h1 className="prog-point__heading">Prog Point: {sessions[0].prog_mech}</h1>
         </main>
     );
 };
