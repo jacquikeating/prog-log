345cb21a0fbb203ec4f8680524bb8e48d2cbd9cb
jacquikeating <j.keating@live.ca>
2026-07-23 21:34:44 -0400

    feat: Add a stats section to ProgPointPage

--- src/pages/ProgPointPage/ProgPointPage.jsx
+++ src/pages/ProgPointPage/ProgPointPage.jsx
@@ -9,20 +9,27 @@ const ProgPointPage = () => {
     const progPhase = latestSession.prog_phase;
     const progPulls = pulls.filter((pull) => pull.mech == progMech);
     const firstSession = sessions.find((session) => session.num == progPulls[0].session_num)
-
     return (
         <main className="prog-point">
             <h1 className="prog-point__heading">
                 Prog Point: 
                 <span className="prog-point__mech">{progMech}</span>
             </h1>
-            <PullsTable
-                pullsArray={progPulls}
-                showEdit={false}
-                progPhase={progPhase}
-                allowDelete={false}
-            />
-=        </main>
+            <section>
+                <p>Phase {progPhase}</p>
+                <p>{progPulls.length} pulls to date</p>
+                <p>First seen in session {firstSession.num} (pull #{progPulls[0].pull_num_overall}), on {firstSession.date}</p>
+                <p>Progging for {latestSession.num - firstSession.num} sessions</p>
+            </section>
+            <section>
+                <PullsTable
+                    pullsArray={progPulls}
+                    showEdit={false}
+                    progPhase={progPhase}
+                    allowDelete={false}
+                />
+            </section>
+        </main>
     );
 };
 
